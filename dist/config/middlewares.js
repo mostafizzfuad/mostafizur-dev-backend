e4f8e4aeb92c6511635626e5f718a45df234be75
Mostafizur <mostafizzfuad@gmail.com>
2026-01-29 01:40:11 +0600

    fix: resolve deployment error

--- dist/config/middlewares.js
+++ dist/config/middlewares.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// config/middlewares.ts
 exports.default = [
     "strapi::logger",
     "strapi::errors",
--- dist/config/server.js
+++ dist/config/server.js
@@ -1,9 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = ({ env }) => ({
-    host: env('HOST', '0.0.0.0'),
-    port: env.int('PORT', 1337),
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
+    // নিচের লাইনটি যোগ করা হলো
+    url: env("URL", "http://localhost:1337"),
     app: {
-        keys: env.array('APP_KEYS'),
+        keys: env.array("APP_KEYS"),
     },
 });
