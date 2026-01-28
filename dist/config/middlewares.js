0b09041cbc7bce910363a7bb4402908765a33e30
Mostafizur <mostafizzfuad@gmail.com>
2026-01-29 00:39:44 +0600

    feat: handle image uploads via Cloudinary

--- dist/config/middlewares.js
+++ dist/config/middlewares.js
@@ -1,14 +1,41 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// config/middlewares.ts
 exports.default = [
-    'strapi::logger',
-    'strapi::errors',
-    'strapi::security',
-    'strapi::cors',
-    'strapi::poweredBy',
-    'strapi::query',
-    'strapi::body',
-    'strapi::session',
-    'strapi::favicon',
-    'strapi::public',
+    "strapi::logger",
+    "strapi::errors",
+    // 'strapi::security',
+    {
+        name: "strapi::security",
+        config: {
+            contentSecurityPolicy: {
+                useDefaults: true,
+                directives: {
+                    "connect-src": ["'self'", "https:"],
+                    "img-src": [
+                        "'self'",
+                        "data:",
+                        "blob:",
+                        "market-assets.strapi.io",
+                        "res.cloudinary.com", // এটি যুক্ত করা জরুরি
+                    ],
+                    "media-src": [
+                        "'self'",
+                        "data:",
+                        "blob:",
+                        "market-assets.strapi.io",
+                        "res.cloudinary.com", // এটি যুক্ত করা জরুরি
+                    ],
+                    upgradeInsecureRequests: null,
+                },
+            },
+        },
+    },
+    "strapi::cors",
+    "strapi::poweredBy",
+    "strapi::query",
+    "strapi::body",
+    "strapi::session",
+    "strapi::favicon",
+    "strapi::public",
 ];
